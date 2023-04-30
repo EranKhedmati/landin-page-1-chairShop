d0eaafd3a6d6404acfea5d8f385a278210983c1c
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2023-04-30 18:56:54 +0430

    add Navbar (its light so its unvisible for now)

--- script/App.js
+++ script/App.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+// Navbar Menu-----------------------------------------------------
+
+// Import elements
+const menu = document.getElementById("menu"),
+    menuBtn = document.getElementById("menu-btn");
+let  menuIsOpen = false;
+
+// Event for toggle menu
+
+menuBtn.addEventListener("click",()=>{
+    if(menuIsOpen) {
+        // handel button
+        menuBtn.classList.add("fa-bars");
+        menuBtn.classList.remove("fa-x");
+        menuBtn.style.color = "var(--light)";
+        // set menu to show
+        menu.style.display = "none";
+        // Tell computer the menu is close now
+        menuIsOpen = false;
+    } else {
+        // handle button
+        menuBtn.classList.remove("fa-bars");
+        menuBtn.classList.add("fa-x");
+        menuBtn.style.color = "var(--black)";
+        // set menu to hidden
+        menu.style.display = "flex";
+        // Tell computer the menu is show now
+        menuIsOpen = true;
+    }
+})
